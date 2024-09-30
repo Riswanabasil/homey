5402360f95e153c2f529f7a8299bad899ac44e2a
Riswanabasil <riswanayacoob@gmail.com>
2024-09-30 12:45:06 +0530

    google

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const dotenv=require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://homeypro.shop/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
